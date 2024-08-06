9b0ad380838479bb93b4759972d9fb6a7193e830
Nailul Ngafwa <fakea4988@gmail.com>
2024-08-06 16:19:35 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./modules/app.module";
 import { Logger } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   process.on("SIGINT", async () => {
     logger.warn("SIGINT signal received: closing HTTP server");
@@ -12,7 +14,7 @@ async function bootstrap() {
     process.exit(0);
   });
 
-  app.enableCors({ origin: true });
+  app.enableCors({ origin: configService.get("CORS_ORIGIN_URL") });
   await app.listen(3000);
   logger.log(`Application is running on: ${await app.getUrl()}`);
 }
